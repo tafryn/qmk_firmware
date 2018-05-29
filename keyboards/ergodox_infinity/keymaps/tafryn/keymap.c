f450f056a1b9003aaeb8d3ad64955cb371a00084
joshru <joshru@uw.edu>
2018-05-29 06:53:07 -0700

    Remove duplicate up keymap, replace with down.

--- keyboards/ergodox_infinity/keymaps/tafryn/keymap.c
+++ keyboards/ergodox_infinity/keymaps/tafryn/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TG(_NUM),            KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,      KC_BSPACE,
                        KC_H,  KC_J,  KC_K,    KC_L,   KC_SCOLON, KC_QUOTE,
   LT(_MOUSE, KC_RBRC), KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC,
-                              RAISE, KC_UP,   KC_UP,  KC_RALT,   KC_RCTL,
+                              RAISE, KC_DOWN, KC_UP,  KC_RALT,   KC_RCTL,
 
   KC_PGDN,    KC_PGUP,
   ALL_OFF,
