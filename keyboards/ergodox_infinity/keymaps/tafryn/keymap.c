271075bf930e1ce59314e90ac27a3ebaf47db23b
tafryn <tafryn@gmail.com>
2018-09-11 08:58:57 -0700

    Revert to normal shift keys.

--- keyboards/ergodox_infinity/keymaps/tafryn/keymap.c
+++ keyboards/ergodox_infinity/keymaps/tafryn/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,     KC_2,    KC_3,     KC_4,  KC_5, KC_GRV,
   KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,  KC_T, KC_DEL,
   KC_LCTL,  KC_A,     KC_S,    KC_D,     KC_F,  KC_G,
-  KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,  KC_B, LT(_MOUSE, KC_LBRC),
+  KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,  KC_B, LT(_MOUSE, KC_LBRC),
   OSM_LCTL, OSM_LALT, KC_LEFT, KC_RIGHT, LOWER,
 
                                                   KC_HOME, KC_END,
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MINS,             KC_6,  KC_7,  KC_8,    KC_9,   KC_0,      KC_EQUAL,
   TG(_NUM),            KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,      KC_BSPACE,
                        KC_H,  KC_J,  KC_K,    KC_L,   KC_SCOLON, KC_QUOTE,
-  LT(_MOUSE, KC_RBRC), KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC,
+  LT(_MOUSE, KC_RBRC), KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
                               RAISE, KC_DOWN, KC_UP,  KC_RALT,   KC_RCTL,
 
   KC_PGDN,    KC_PGUP,
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,     KC_2,    KC_3,     KC_4,  KC_5, KC_GRV,
   KC_TAB,   KC_QUOT,  KC_COMM, KC_DOT,   KC_P,  KC_Y, KC_DEL,
   KC_LCTL,  KC_A,     KC_O,    KC_E,     KC_U,  KC_I,
-  KC_LSPO,  KC_SCLN,  KC_Q,    KC_J,     KC_K,  KC_X, LT(_MOUSE, KC_LBRC),
+  KC_LSFT,  KC_SCLN,  KC_Q,    KC_J,     KC_K,  KC_X, LT(_MOUSE, KC_LBRC),
   OSM_LCTL, OSM_LALT, KC_LEFT, KC_RIGHT, LOWER,
 
                                                 KC_HOME, KC_END,
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQUAL,             KC_6, KC_7,  KC_8,    KC_9,     KC_0, KC_BSLS,
   TG(_NUM),             KC_F, KC_G,  KC_C,    KC_R,     KC_L, KC_SLSH,
                         KC_D, KC_H,  KC_T,    KC_N,     KC_S, KC_MINS,
-  LT(_MOUSE, KC_RBRC),  KC_B, KC_M,  KC_W,    KC_V,     KC_Z, KC_RSPC,
+  LT(_MOUSE, KC_RBRC),  KC_B, KC_M,  KC_W,    KC_V,     KC_Z, KC_RSFT,
                               RAISE, KC_DOWN, KC_UP, KC_LALT, KC_RCTL,
 
   KC_PGDN,    KC_PGUP,
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,     KC_2,    KC_3,    KC_4,  KC_5, KC_GRV,
   KC_TAB,   KC_Q,     KC_W,    KC_F,    KC_P,  KC_G, KC_DEL,
   KC_LCTL,  KC_A,     KC_R,    KC_S,    KC_T,  KC_D,
-  KC_LSPO,  KC_Z,     KC_X,    KC_C,    KC_V,  KC_B, LT(_MOUSE, KC_LBRC),
+  KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,  KC_B, LT(_MOUSE, KC_LBRC),
   OSM_LCTL, OSM_LALT, KC_LEFT, KC_RGHT, LOWER,
 
                                                   KC_HOME, KC_END,
@@ -174,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLS,             KC_6, KC_7,  KC_8,    KC_9,   KC_0,    KC_EQUAL,
   TG(_NUM),            KC_J, KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_MINS,
                        KC_H, KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT,
-  LT(_MOUSE, KC_RBRC), KC_K, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+  LT(_MOUSE, KC_RBRC), KC_K, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                              RAISE, KC_DOWN, KC_UP,  KC_RALT, KC_RCTL,
 
   KC_PGDN,    KC_PGUP,
@@ -208,7 +208,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,     KC_2,    KC_3,    KC_4,  KC_5, KC_MINS,
   KC_TAB,   KC_Q,     KC_D,    KC_R,    KC_W,  KC_B, KC_DEL,
   KC_LCTL,  KC_A,     KC_S,    KC_H,    KC_T,  KC_G,
-  KC_LSPO,  KC_Z,     KC_X,    KC_M,    KC_C,  KC_V, LT(_MOUSE, KC_LBRC),
+  KC_LSFT,  KC_Z,     KC_X,    KC_M,    KC_C,  KC_V, LT(_MOUSE, KC_LBRC),
   OSM_LCTL, OSM_LALT, KC_LEFT, KC_RGHT, LOWER,
 
                                                   KC_HOME, KC_END,
@@ -219,7 +219,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLS,             KC_6, KC_7,  KC_8,    KC_9,   KC_0,      KC_EQUAL,
   TG(_NUM),            KC_J, KC_F,  KC_U,    KC_P,   KC_SCOLON, KC_BSPACE,
                        KC_Y, KC_N,  KC_E,    KC_O,   KC_I,      KC_QUOTE,
-  LT(_MOUSE, KC_RBRC), KC_K, KC_L,  KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC,
+  LT(_MOUSE, KC_RBRC), KC_K, KC_L,  KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
                              RAISE, KC_DOWN, KC_UP,  KC_RALT,   KC_RCTL,
 
   KC_PGDN,    KC_PGUP,
